b7764fa088d6185d0e955471f22c07f174163cc4
Hugo530 <101437827+Hugo530@users.noreply.github.com>
2022-05-04 14:40:04 -0700

    created models for category,product,tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,14 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Getting all the categroies
+router.get('/', async (req, res) => {
+ try{
+   const categoryData = await Category.findAll();
+   res.status(200).json(travellerData);
+ } catch(err) {
+   res.status(500).json(err);
+ }
 });
 
 router.get('/:id', (req, res) => {
